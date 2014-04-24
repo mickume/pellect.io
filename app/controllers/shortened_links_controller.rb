0368d04a459374f418170290525877fdb7d77579
Michael Kuehl <mickuehl@googlemail.com>
2014-04-24 21:54:35 +0200

    added link TTL calculation

--- app/controllers/shortened_links_controller.rb
+++ app/controllers/shortened_links_controller.rb
@@ -3,15 +3,19 @@ class ShortenedLinksController < ApplicationController
     b = Bookmark.find_by_url_shortened(params[:short_url])
 
     if b && (b.user_id == current_user.id)
-      # don't want to wait for the increment to happen, make it snappy!
-      Thread.new do
-      # this is the place to enhance the metrics captured
-      # for the system. You could log the request origin
-      # browser type, ip address etc.
-        b.increment(:view_count)
-        b.save
+      
+      # increment the view count
+      b.increment(:view_count)
+      
+      # add some days to prevent the bookmark from expiring 
+      if b.time_to_expiration < Figaro.env.url_ttl.to_i
+        if b.view_count < 7
+          b.time_to_expiration = b.time_to_expiration + (2 ** b.view_count)
+        end
       end
-
+      
+      b.save
+      
       # retrieve and redirect to the 'real' url
       redirect_to Resource.find(b.resource_id).url
 
