af5bb73425f6baab77edeca4af2f70d9ad6c4603
Michael Kuehl <mickuehl@googlemail.com>
2014-04-24 21:58:58 +0200

    added link TTL calculation

--- app/controllers/shortened_links_controller.rb
+++ app/controllers/shortened_links_controller.rb
@@ -7,9 +7,10 @@ class ShortenedLinksController < ApplicationController
       # increment the view count
       b.increment(:view_count)
       
-      # add some days to prevent the bookmark from expiring 
-      if b.time_to_expiration < Figaro.env.url_ttl.to_i
-        if b.view_count < 7
+      # add some days to prevent the bookmark from expiring
+      ttl = Figaro.env.url_ttl.to_i
+      if b.time_to_expiration < (ttl * 2)
+        if b.view_count < 8
           b.time_to_expiration = b.time_to_expiration + (2 ** b.view_count)
         end
       end
