9fc5858394e130d17028e3d77cb1df208785f1f6
Michael Kuehl <michael@ratchet.cc>
2018-01-30 15:11:09 +0100

    rake db fu

--- bin/migrate-database.sh
+++ bin/migrate-database.sh
@@ -2,4 +2,6 @@
 set -e
 
 echo "Executing bundle exec 'rake db:migrate' ..."
+RAILS_ENV=production bundle exec rake db:drop
+RAILS_ENV=production bundle exec rake db:create
 RAILS_ENV=production bundle exec rake db:migrate
