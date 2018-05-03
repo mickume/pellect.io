f26f54654a9c2b6c2b0d2159c3b2a85ba521369b
Michael Kuehl <michael@ratchet.cc>
2018-05-03 17:03:57 +0200

    changes to application controller due to a crash-loop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :blocked_user?
   
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
 
-  def blocked_user?
-    user_is_locked? current_user
-  end
-  
 end
