b67e1ee493df228dc4279526e180789b6f93391a
Michael Kuehl <michael@ratchet.cc>
2018-05-03 13:10:47 +0200

    some porting from saas-starter-app

--- bin/migrate-database.sh
+++ bin/migrate-database.sh
@@ -2,6 +2,4 @@
 set -e
 
 echo "Executing bundle exec 'rake db:migrate' ..."
-RAILS_ENV=production bundle exec rake db:drop
-RAILS_ENV=production bundle exec rake db:create
 RAILS_ENV=production bundle exec rake db:migrate
